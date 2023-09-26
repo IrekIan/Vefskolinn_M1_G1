3fc616de3566908c2609443688942ce3f5b52585
Irek <ir.jn.kr@gmmail.com>
2023-09-26 00:41:07 +0000

    hgw

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const button = document.querySelector('.title-bottom');
 const body = document.querySelector('.section');
+const poster = document.querySelector('.poster');
 
 const strobo = () => {
 	body.classList.toggle('body-strobo');
+	poster.classList.toggle('poster-strobo');
 	console.log('test');
 };
 
